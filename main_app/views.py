bdce8e808b8401cea8e6cef7e36cf2687c83d79d
j salvador navarro <salvador.navarro@gmail.com>
2019-03-22 09:47:13 -0500

    adding views.py to the commit

--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from datetime import date
 import uuid
 import boto3
 from .models import Company, Meal, Photo
